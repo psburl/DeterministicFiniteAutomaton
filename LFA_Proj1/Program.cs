5cbd4016373ebf93e2fcd8e1bcd1fd151a35e4f1
psburl <wesley.burlani@hub2b.com.br>
2017-05-02 18:32:33 -0300

    created root project on core

---
+++ LFA_Proj1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LFA_Proj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
